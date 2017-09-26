7e733390253b588a5768e01b5cde8bdb8b118904
aliciapflaumer <alpfla7@gmail.com>
2017-09-26 18:55:52 -0400

    Created Status column to table
    - the user can update their reading status for each book

--- scripts/books/create_book.sh
+++ scripts/books/create_book.sh
@@ -1,5 +1,5 @@
 curl --include --request POST "http://localhost:4741/books" \
-# --header "Authorization: Token token=${TOKEN}" \
+--header "Authorization: Token token=${TOKEN}" \
 --header "Content-Type: application/json" \
 --data '{
   "book": {
