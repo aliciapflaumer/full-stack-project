2c1d5aa9312e12f5117174a08cdfbbff899f2ba2
aliciapflaumer <alpfla7@gmail.com>
2017-09-27 10:42:11 -0400

    Change to curl script for update
    Testing to fix 400 Bad Request error in web ui

--- scripts/books/create_book.sh
+++ scripts/books/create_book.sh
@@ -1,9 +1,10 @@
 curl --include --request POST "http://localhost:4741/books" \
---header "Authorization: Token token=${TOKEN}" \
+--header "Authorization: Token token=BAhJIiU5YjNjY2I2NTVmMjQxZDc5MDlmM2IxZWZhNjkxODllNgY6BkVG--906f7d9dd884311c78ded38a4a2b77999a73e654" \
 --header "Content-Type: application/json" \
 --data '{
   "book": {
-    "title": "'"${TITLE}"'",
-    "author": "'"${AUTHOR}"'"
+    "title": "Game of Thrones",
+    "author": "George R. R. Martin",
+    "newstatus": "I read it"
   }
 }'
